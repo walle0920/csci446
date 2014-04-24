ab4240c9629cd103fddcba863532f41c1cd4bc0f
hxiao <hxiao@mymail.mines.edu>
2014-04-23 22:48:18 -0600

    10.02

--- depot/app/controllers/line_items_controller.rb
+++ depot/app/controllers/line_items_controller.rb
@@ -27,10 +27,13 @@ class LineItemsController < ApplicationController
     @cart = current_cart
     product = Product.find(params[:product_id])
     @line_item = @cart.add_product(product.id)
+    @line_item.product = product
+   
 
     respond_to do |format|
       if @line_item.save
         format.html { redirect_to(store_url) }
+        format.js   { @current_item = @line_item }
         format.json { render action: 'show', status: :created, location: @line_item }
       else
         format.html { render action: 'new' }
